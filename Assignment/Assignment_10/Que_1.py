d1f91a6b9cdfd0d888c2e70f541e52fea83c7c10
Ashwini Bagadi <ashwinibagadi4@gmail.com>
2025-12-29 10:16:17 +0530

    Assignment-10 updated

--- Assignment/Assignment_10/Que_1.py
+++ Assignment/Assignment_10/Que_1.py
@@ -1,19 +1,25 @@
-# 1. Implement the chunking examples discussed in the provided PDF document.
-# *Optional Question*
+# Create a Streamlit web application that allows users to connect to a MySQL database and ask natural language questions. The app should generate and execute SELECT SQL queries using an LLM and display both the query results and a simple English explanation.
 
-# Create a Streamlit web application that allows users to connect to a 
-# MySQL database and ask natural language questions. 
-# The app should generate and execute SELECT SQL queries using an LLM 
-# and display both the query results and a simple English explanation.
 # Note:
-# Use the sample MySQL connection parameters provided in connection.txt 
-# and the sample database schema in db.txt for testing.
+# Use the sample MySQL connection parameters provided in connection.txt and the sample database schema in db.txt for testing.
+
 # pip install mysql-connector-python
 
-import streamlit as st
-from langchain_text_splitters import CharacterTextSplitter
+import mysql.connector
 
-text_splitter = CharacterTextSplitter(chunk_size=500,chunk_overlap=50)
-docs = text_splitter.create_documents(["Gen Ai is leading area in IT sector , it replace humans but create new opportunity"])
-st.write(docs)
-print(docs)
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="@Bhargavi45",
+    database="cdac"
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM employees")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
